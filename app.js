b8afc0b623e8e1edbf24253398803019f733bf0d
robiwankenobi <rfnkurle@gmail.com>
2019-04-24 18:02:40 -0700

    Scraper 1.0

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var exphbs= require("express-handlebars");
 var mongoose= require("mongoose");
 var axios = require ("axios")
 var cheerio = require("cheerio")
-var Note = require("./models/Note.js");
-var Article = require("./models/Article.js");
+var db = require("./models");
 
 var PORT = process.env.PORT || 8080
 
@@ -37,26 +36,40 @@ app.get("/article", function(req, res){
 axios.get("https://www.theonion.com/").then(function(response){
         var $ = cheerio.load(response.data);
         
-        var results =[];
+        var results ={
+            articles: []
+        };
         $(".js_post_item").each(function(i, element) {
             
-             var link = $(element).children("header").children("h1").children("a").attr("href");
-            var title = $(element).children("header").children("h1").children("a").text();
-            var summary = $(element).children(".js_item-content").children(".entry-summary").children("p").text();
+             results.link = $(this).children("header").children("h1").children("a").attr("href");
+            results.title = $(this).children("header").children("h1").children("a").text();
+            results.summary = $(this).children(".js_item-content").children(".entry-summary").children("p").text();
             // Save these results in an object that we'll push into the results array
-            results.push({
-              title: title,
-              link: link,
-              summary: summary
-              
-            });
+            
+            results.articles.push({
+                title: results.title,
+                link: results.link,
+                summary: results.summary,
+                
+              });
+            
+            db.Article.create(results)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, log it
+          console.log(err);
+        });
+         
             // console.log(results)
                         
         });
-        res.render("index", {results});
+       
 
         // res.json(results)
-        
+        res.render("index", {results});
          
         });
         
@@ -109,72 +122,72 @@ app.post("/article/:id", function(req, res) {
 
   
 
-//   // post route to save an atricle to the database
-//   app.post("/savedarticles", function(req, res) {
-//     db.Article.create({
-//       title: req.body.title,
-//       link: req.body.link,
-//       summary: req.body.summary,
+  // post route to save an atricle to the database
+  app.post("/savedarticles", function(req, res) {
+    db.Article.create({
+      title: req.body.title,
+      link: req.body.link,
+      summary: req.body.summary,
       
-//     }).then(function(dbArticle){
-//       res.json(dbArticle);
-//     }).catch(function(err){
-//       res.json(err);
-//     });
-//   });
+    }).then(function(dbArticle){
+      res.json(dbArticle);
+    }).catch(function(err){
+      res.json(err);
+    });
+  });
 
-//   // delete route to remove a saved article from the database
-//   app.delete("/savedarticles", function(req, res) {
-//     // first remove any note associated with the article
-//     db.Note.remove({
-//       article: req.body.id
-//     // then remove the article itself
-//     }).then(function(dbNote){
-//       return db.Article.remove({_id: req.body.id})
-//     // respond to the browser
-//     }).then(function(dbArticle){
-//       res.json(dbArticle);
-//     }).catch(function(err){
-//       res.json(err);
-//     });
-//   });
+  // delete route to remove a saved article from the database
+  app.delete("/savedarticles", function(req, res) {
+    // first remove any note associated with the article
+    db.Note.remove({
+      article: req.body.id
+    // then remove the article itself
+    }).then(function(dbNote){
+      return db.Article.remove({_id: req.body.id})
+    // respond to the browser
+    }).then(function(dbArticle){
+      res.json(dbArticle);
+    }).catch(function(err){
+      res.json(err);
+    });
+  });
 
-//   // get route to retrieve the comments associated with a specified article
-//   app.get("/savednotes", function(req, res) {
-//     db.Note.find({
-//       article: req.query.articleId
-//     }).then(function(dbNote) {
-//       res.json(dbNote);
-//     }).catch(function(err) {
-//       res.json(err);
-//     });
-//   });
+  // get route to retrieve the comments associated with a specified article
+  app.get("/savednotes", function(req, res) {
+    db.Note.find({
+      article: req.query.articleId
+    }).then(function(dbNote) {
+      res.json(dbNote);
+    }).catch(function(err) {
+      res.json(err);
+    });
+  });
 
-//   // post route to add a comment to the specified article
-//   app.post("/savednotes", function(req, res) {
-//     db.Note.create({
-//       body: req.body.body,
-//       article: req.body.article
-//     }).then(function(dbNote){
-//       res.json(dbNote);
-//     }).catch(function(err){
-//       res.json(err);
-//     });
-//   });
+  // post route to add a comment to the specified article
+  app.post("/savednotes", function(req, res) {
+    db.Note.create({
+      body: req.body.body,
+      article: req.body.article
+    }).then(function(dbNote){
+      res.json(dbNote);
+    }).catch(function(err){
+      res.json(err);
+    });
+  });
 
-//   // delete route to remove a comment
-//   app.delete("/savednotes", function(req, res) {
-//     db.Note.remove({
-//       _id: req.body.id
-//     }, function(err, data){
-//       if (err) {
-//         res.send(500);
-//         console.log(err);
-//       } else {
-//         res.json(data);
-//       };
-//     });
-//   });
+  // delete route to remove a comment
+  app.delete("/savednotes", function(req, res) {
+    db.Note.remove({
+      _id: req.body.id
+    }, function(err, data){
+      if (err) {
+        res.send(500);
+        console.log(err);
+      } else {
+        res.json(data);
+      };
+    });
+  });
 
 
  
